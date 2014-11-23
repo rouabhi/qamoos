8840d9459d4207c60190e233f4ea2775637a2f2a
ROUABHI <git@wikeo.mobi>
2014-11-23 20:25:07 +0100

    qamoos in server variable and lang stored in session variable

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 ﻿var supportedLang=["fr","en","ar"],
-	currentLang="en";
+	varSession;
 
-module.exports = {define:addQamoos, get:getQamoos, lang:setLang}
+module.exports = function(req) {
+	varSession = require("varsession")(req);
+	return {define:addQamoos, get:getQamoos, lang:setLang};
+}
 
 function addQamoos( name ){
 	if (!global['qamoos']) global['qamoos']={};
-	if (typeof global['qamoos'][name] == "undefined") {
+	if (!global['qamoos'][name]) {
 		global['qamoos'][name]={
 			type:'Qamoos',
 			data:{}
@@ -19,7 +22,7 @@ function getQamoos(name){
 
 	var qamoos = global['qamoos'][name];
 	if (!qamoos) return null;
-	return {set:setMsg.bind(qamoos), get:getMsg.bind(qamoos), getErr:getMsgAsErr.bind(qamoos)};
+	return {set:setMsg.bind(qamoos), get:getMsg.bind(qamoos), getErr:getMsgAsErr.bind(qamoos), lang:setLang};
 }
 
 function setMsg(index, translations, code){
@@ -36,27 +39,30 @@ function setMsg(index, translations, code){
 }
 
 function getMsg(index){
-	var qamoos=this, entry=qamoos.data[index];
+	var qamoos=this, entry=qamoos.data[index],
+		lang = varSession.get("qamoosLang","en");
 
 	if (!entry) return null;
-	if (entry[currentLang]) return entry[currentLang];
-	for(var lang in entry) if (typeof entry[lang]=="string") return entry[lang];  
+	if (entry[lang]) return entry[lang];
+	for(lang in entry) if (typeof entry[lang]=="string") return entry[lang];  
 	return "";
 }
 
 function getMsgAsErr(index){
-	var qamoos=this, entry=qamoos.data[index],code;
+	var qamoos=this, entry=qamoos.data[index],
+		code,
+		lang = varSession.get("qamoosLang","en");
 
 	if (!entry) return null;
 	code=(typeof entry.code !== "undefined") ? entry.code:0;
 
-	if (entry[currentLang]) return {err: code, msg:entry[currentLang]};
-	for(var lang in entry) if (typeof entry[lang]=="string") return {err:code, msg:entry[lang]};
+	if (entry[lang]) return {err: code, msg:entry[lang]};
+	for(lang in entry) if (typeof entry[lang]=="string") return {err:code, msg:entry[lang]};
 	return {err:code, msg:""};
 }
 
 
 function setLang(lang){
- if ((typeof lang == 'string')&&(supportedLang.indexOf(lang)>=0)) {currentLang=lang;return true;}
+ if (supportedLang.indexOf(lang)>=0) {varSession.set("qamoosLang",lang); return true}
  return null;
 }
